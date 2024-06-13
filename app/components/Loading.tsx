526bef962351d767540eb5bfbd73a4181a7297aa
Paul Irungu <paul.irungu@gmail.com>
2024-06-13 13:10:40 +0300

    first commit

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,8 +1,9 @@
 // components/Loading.tsx
 const Loading: React.FC = () => {
   return (
-    <div className="flex bg-gray-800 items-center justify-center h-screen">
+    <div className="flex items-center bg-gray-900 justify-center h-screen relative">
       <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-400"></div>
+      <div className="absolute text-gray-400 text-xs">Game loading...</div>
     </div>
   );
 };
