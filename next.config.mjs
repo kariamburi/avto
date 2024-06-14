ffc5603e174709aa5ebb5fd8eeaba1d50501c709
Paul Irungu <paul.irungu@gmail.com>
2024-06-14 18:21:40 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,17 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "X-Forwarded-Proto", value: "https" }
+          ]
+        }
+      ]
+    }
+  }
+  
