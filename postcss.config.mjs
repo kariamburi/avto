2653e4f931677281e0f4c45f449e9fd442df2c5f
Paul Irungu <paul.irungu@gmail.com>
2024-06-30 11:51:02 +0300

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,15 +2,6 @@
 const config = {
   plugins: {
     tailwindcss: {},
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {},
-    '@fullhuman/postcss-purgecss': {
-      content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}',
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    },
   },
 };
 
