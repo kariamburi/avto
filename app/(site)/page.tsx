58135d18049580252fa80bc0c1e1ada254508600
Paul Irungu <paul.irungu@gmail.com>
2024-08-04 16:47:04 +0300

    first commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { useState, useEffect } from "react";
-import Game from "../components/Game";
+import dynamic from "next/dynamic";
 import { Toaster } from "@/components/ui/toaster";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "../components/navbar";
-import Loading from "../components/Loading";
+
+const Game = dynamic(() => import("../components/Game"), { ssr: false });
+const Navbar = dynamic(() => import("../components/navbar"), { ssr: false });
+const Loading = dynamic(() => import("../components/Loading"), { ssr: false });
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
   const [progress, setProgress] = useState(0);
+
   useEffect(() => {
     const totalLoadingTime = 2000; // Total loading time in milliseconds
     const updateInterval = 100; // Update interval in milliseconds
@@ -32,12 +35,14 @@ export default function Home() {
       clearInterval(interval);
     };
   }, []);
+
   if (isLoading) {
     return <Loading progress={progress} />;
   }
 
   return (
     <main>
+      <Navbar />
       <Game />
       <Toaster />
     </main>
