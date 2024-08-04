933614312ac916b996ae60dd2c6f76d31f5dc08d
Paul Irungu <paul.irungu@gmail.com>
2024-08-04 16:32:19 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import { PHASE_PRODUCTION_BUILD } from 'next/constants';
+import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
