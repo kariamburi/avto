40bcf5d10e5eecaf84798d8a3a24fbde97e2a19d
kariamburi <paul.irungu@gmail.com>
2024-10-28 11:57:35 +0300

    Update server.js
    ok

--- server.js
+++ server.js
@@ -208,13 +208,13 @@ app.prepare().then(async () => {
 //  ssl_certificate     /home/aviator/conf/web/aviatorgm.com/ssl/aviatorgm.com.pem;
  // ssl_certificate_key /home/aviator/conf/web/aviatorgm.com/ssl/aviatorgm.com.key;
 
- const sslOptions = {
-   key: fs.readFileSync('/home/aviator/conf/web/aviatorgm.com/ssl/aviatorgm.com.key'),
-   cert: fs.readFileSync('/home/aviator/conf/web/aviatorgm.com/ssl/aviatorgm.com.pem')
-  };
+ //const sslOptions = {
+ //  key: fs.readFileSync('/home/aviator/conf/web/aviatorgm.com/ssl/aviatorgm.com.key'),
+ //  cert: fs.readFileSync('/home/aviator/conf/web/aviatorgm.com/ssl/aviatorgm.com.pem')
+ // };
 
-  const httpsServer = https.createServer(sslOptions, server);
- // const httpsServer = http.createServer(server);
+ // const httpsServer = https.createServer(sslOptions, server);
+  const httpsServer = http.createServer(server);
 
   const wss = new WebSocket.Server({ noServer: true });
 
