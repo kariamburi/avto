372dd1d9a7840b3b1c0a924e5ec91d178b609d88
kariamburi <paul.irungu@gmail.com>
2024-10-28 17:48:18 +0300

    Update useWebSocket.ts

--- app/hooks/useWebSocket.ts
+++ app/hooks/useWebSocket.ts
@@ -11,7 +11,7 @@ const useWebSocket = () => {
   
   useEffect(() => {
     //const ws = new WebSocket('wss://23.227.167.127:3001/ws');
-    const ws = new WebSocket('wss://aviatorgm.com/ws');
+    const ws = new WebSocket('wss://aviatorgm.com:3001/ws');
     //const ws = new WebSocket('ws://23.227.167.127:3001/ws');
     setSocket(ws);
 
